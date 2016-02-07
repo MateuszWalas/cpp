a1524195b3a9669036c563e37f8c1053b96ddf35
Mateusz <mateusz.walas@protonmail.com>
2016-02-07 06:14:35 +0100

    Doing damage corrected.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(void){
-    Character* player = new Character("Mateusz");    
+    Character* player = new Character("Player 1");    
     player->hello();
     player->attack(player);
     player->attack(player);
