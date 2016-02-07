93fafb6008a29e65f3228afc05d996a66a1e272e
Mateusz <mateusz.walas@protonmail.com>
2016-02-07 06:12:30 +0100

    Doing damage corrected.

--- character.h
+++ character.h
@@ -7,18 +7,15 @@ using namespace std;
 
 class Character {
 	private:
-		int _HP, _HP_MAX;
-	    int _MANA, _MANA_MAX;
+        int _HP, _HP_MAX;
+        int _MANA, _MANA_MAX;
         int _POWER, _POWER_MAX;
         int _DEFENCE, _DEFENCE_MAX;
         string _NICKNAME;
-        
     public:
-		Character(string = "Someone");
-        
+        Character(string = "Someone");
         void hello();
         void attack(Character* player);
-
 };
 
 #endif
